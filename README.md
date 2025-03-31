ae4294c5a277052c8c09e8ceeb9de0cae6cb26b4
Jan Maciejewski <u162_janmac_waw@technischools.com>
2025-03-31 14:44:53 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# si-projekt-memory
---
+++ script.js
@@ -0,0 +1,69 @@
+const cards = document.querySelectorAll(".card");
+const cardImages = [
+  "./assets/cat1.jpg",
+  "./assets/cat1.jpg",
+  "./assets/cat2.jpg",
+  "./assets/cat2.jpg",
+  "./assets/cat3.jpg",
+  "./assets/cat3.jpg",
+  "./assets/cat4.jpg",
+  "./assets/cat4.jpg",
+  "./assets/cat5.jpg",
+  "./assets/cat5.jpg",
+  "./assets/cat6.jpg",
+  "./assets/cat6.jpg",
+  "./assets/cat7.jpg",
+  "./assets/cat7.jpg",
+  "./assets/cat8.jpg",
+  "./assets/cat8.jpg",
+];
+
+let flippedCards = [];
+let matchedCards = [];
+
+const shuffledImages = [...cardImages].sort(() => Math.random() - 0.5);
+
+cards.forEach((card, index) => {
+  card.addEventListener("click", () => flipCard(card, index));
+});
+
+function flipCard(card, index) {
+  if (
+    flippedCards.length >= 2 ||
+    card.classList.contains("flipped") ||
+    card.classList.contains("matched")
+  ) {
+    return;
+  }
+
+  card.classList.add("flipped");
+  card.style.backgroundImage = `url(${shuffledImages[index]})`;
+  flippedCards.push(card);
+
+  if (flippedCards.length === 2) checkMatch();
+}
+
+function checkMatch() {
+  const [firstCard, secondCard] = flippedCards;
+
+  if (firstCard.style.backgroundImage === secondCard.style.backgroundImage) {
+    firstCard.classList.add("matched");
+    secondCard.classList.add("matched");
+    matchedCards.push(firstCard, secondCard);
+    flippedCards = [];
+
+    if (matchedCards.length === cards.length) {
+      setTimeout(() => {
+        alert("wygrałeś!!!");
+      }, 1000);
+    }
+  } else {
+    setTimeout(() => {
+      firstCard.classList.remove("flipped");
+      secondCard.classList.remove("flipped");
+      firstCard.style.backgroundImage = "";
+      secondCard.style.backgroundImage = "";
+      flippedCards = [];
+    }, 1000);
+  }
+}
